0a863e9e8b78ea7970b24d0b163232ed27bae5bf
Austen Wood <50690066+austenwood@users.noreply.github.com>
2019-11-03 19:35:48 -0800

    Change srun to mpirun

--- code/slurmscript.sh
+++ code/slurmscript.sh
@@ -10,4 +10,4 @@ python3 -m pip install mpi4py --user
 python3 -m pip install numpy --user
 module load mpi/openmpi-x86_64
 python3 -c "import mpi4py"
-srun python3 helloworld.py
+mpirun python3 helloworld.py
