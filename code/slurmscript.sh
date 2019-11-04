9b13d4e7b524cbc9682ac9acb2821362e4ac5566
Austen Wood <50690066+austenwood@users.noreply.github.com>
2019-11-03 18:09:36 -0800

    Add support for Python

--- code/slurmscript.sh
+++ code/slurmscript.sh
@@ -1,20 +1,13 @@
-#!/bin/sh
-#SBATCH --nodes=2
-#SBATCH --job-name="hello_test"
-#SBATCH --output=test-srun.out
-# The initial srun will trigger the SLURM prologue on the compute nodes.
-echo "SLURM_JOBID="$SLURM_JOBID
-echo "SLURM_JOB_NODELIST"=$SLURM_JOB_NODELIST
-echo "SLURM_NNODES"=$SLURM_NNODES
-echo "SLURMTMPDIR="$SLURMTMPDIR
-echo "working directory = "$SLURM_SUBMIT_DIR
-NPROCS=`srun --nodes=${SLURM_NNODES} bash -c 'hostname' |wc -l`
-echo NPROCS=$NPROCS
-srun --nodes=${SLURM_NNODES} bash -c 'hostname'
-echo "Launch helloworld with srun"
-
-#The PMI library is necessary for srun
-export I_MPI_PMI_LIBRARY=/usr/lib64/libpmi.so
-srun ./hello
+#!/bin/bash
 #
-echo "All Done!"
+#SBATCH --job-name=gameoflife
+#SBATCH --output=output.txt
+#
+#SBATCH --nodes=2
+
+python3 -m pip install --upgrade pip --user
+python3 -m pip install mpi4py --user
+python3 -m pip install numpy --user
+module load mpi/openmpi-x86_64
+python -c "import mpi4py"
+srun python3 helloworld.py
