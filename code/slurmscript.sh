c353f2482ac69c2b754146c4fbe3664f929a6fec
Austen Wood <50690066+austenwood@users.noreply.github.com>
2019-11-03 19:12:02 -0800

    Change python to python3

--- code/slurmscript.sh
+++ code/slurmscript.sh
@@ -9,5 +9,5 @@ python3 -m pip install --upgrade pip --user
 python3 -m pip install mpi4py --user
 python3 -m pip install numpy --user
 module load mpi/openmpi-x86_64
-python -c "import mpi4py"
+python3 -c "import mpi4py"
 srun python3 helloworld.py
